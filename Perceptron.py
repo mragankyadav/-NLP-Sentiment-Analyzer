2803710c845c392066b3deae1758255280952b34
mragank <mragank@tamu.edu>
2017-02-28 23:42:59 -0600

    NaiveBayes and Perceptron Approach

---
+++ Perceptron.py
@@ -0,0 +1,252 @@
+from __future__ import division
+import sys
+import getopt
+import os
+import math
+import operator
+from collections import Counter
+import random
+random.seed(0)
+
+class Perceptron:
+  class TrainSplit:
+    """Represents a set of training/testing data. self.train is a list of Examples, as is self.test. 
+    """
+    def __init__(self):
+      self.train = []
+      self.test = []
+
+  class Example:
+    """Represents a document with a label. klass is 'pos' or 'neg' by convention.
+       words is a list of strings.
+    """
+    def __init__(self):
+      self.klass = ''
+      self.words = []
+
+
+  def __init__(self):
+    """Perceptron initialization"""
+    #in case you found removing stop words helps.
+    self.stopList = set(self.readFile('../data/english.stop'))
+    self.numFolds = 10
+    self.wA={}
+    self.bias_avg=0
+    self.w0={}
+    self.curr_bias=0
+    self.steps=1
+    self.g=0
+
+  #############################################################################
+  # TODO TODO TODO TODO TODO 
+  # Implement the Perceptron classifier with
+  # the best set of features you found through your experiments with Naive Bayes.
+
+  def classify(self, words):
+    """ TODO
+      'words' is a list of words to classify. Return 'pos' or 'neg' classification.
+    """
+
+    testVector={}
+    for i in words:
+      if testVector.get(i)==None:
+        testVector[i]=1
+      else:
+        testVector[i]+=1
+    total=0
+    for k,v in testVector.iteritems():
+      if self.w0.get(k)!=None:
+        total+=(self.w0[k]-self.wA[k]/self.steps)*testVector[k]
+    total+=self.curr_bias-(self.bias_avg/self.steps)
+
+
+    if total<0:
+      return "neg"
+    else:
+      return "pos"
+
+  
+
+  def addExample(self, klass, words):
+    """
+     * TODO
+     * Train your model on an example document with label klass ('pos' or 'neg') and
+     * words, a list of strings.
+     * You should store whatever data structures you use for your classifier 
+     * in the Perceptron class.
+     * Returns nothing
+    """
+    if klass=="pos":
+      y=1
+    else:
+      y=-1
+    frequency=Counter(words)
+    if self.g==0:
+      for i in frequency:
+        if self.w0.get(i)==None:
+          self.w0[i]=0
+          self.wA[i]=0
+    total = 0
+    for word in frequency:
+      total += (frequency[word]) * self.w0[word]
+    total+=self.curr_bias
+    # print total
+    if y*total<=0:
+      for word in frequency:
+        self.w0[word]+=y*frequency[word]
+        self.wA[word]+=y*frequency[word]*self.steps
+      self.bias_avg+=y*self.steps
+      self.curr_bias += y
+    self.steps+=1
+
+
+
+  
+  def train(self, split, iterations):
+      """
+      * TODO 
+      * iterates through data examples
+      * TODO 
+      * use weight averages instead of final iteration weights
+      """
+      for i in range(iterations):
+        for example in split.train:
+            words = example.words
+            words = self.filterStopWords(words)
+            self.addExample(example.klass, words)
+        self.g+=1
+
+      
+
+  # END TODO (Modify code beyond here with caution)
+  #############################################################################
+  
+  
+  def readFile(self, fileName):
+    """
+     * Code for reading a file.  you probably don't want to modify anything here, 
+     * unless you don't like the way we segment files.
+    """
+    contents = []
+    f = open(fileName)
+    for line in f:
+      contents.append(line)
+    f.close()
+    result = self.segmentWords('\n'.join(contents)) 
+    return result
+
+  
+  def segmentWords(self, s):
+    """
+     * Splits lines on whitespace for file reading
+    """
+    return s.split()
+
+  
+  def trainSplit(self, trainDir):
+    """Takes in a trainDir, returns one TrainSplit with train set."""
+    split = self.TrainSplit()
+    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
+    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
+    for fileName in posTrainFileNames:
+      example = self.Example()
+      example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+      example.klass = 'pos'
+      split.train.append(example)
+    for fileName in negTrainFileNames:
+      example = self.Example()
+      example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+      example.klass = 'neg'
+      split.train.append(example)
+    return split
+
+
+  def crossValidationSplits(self, trainDir):
+    """Returns a lsit of TrainSplits corresponding to the cross validation splits."""
+    splits = [] 
+    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
+    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
+    #for fileName in trainFileNames:
+    for fold in range(0, self.numFolds):
+      split = self.TrainSplit()
+      for fileName in posTrainFileNames:
+        example = self.Example()
+        example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+        example.klass = 'pos'
+        if fileName[2] == str(fold):
+          split.test.append(example)
+        else:
+          split.train.append(example)
+      for fileName in negTrainFileNames:
+        example = self.Example()
+        example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+        example.klass = 'neg'
+        if fileName[2] == str(fold):
+          split.test.append(example)
+        else:
+          split.train.append(example)
+      random.shuffle(split.train)
+      splits.append(split)
+    return splits
+  
+  
+  def filterStopWords(self, words):
+    """Filters stop words."""
+    filtered = []
+    for word in words:
+      if not word in self.stopList and word.strip() != '':
+        filtered.append(word)
+    return filtered
+
+def test10Fold(args):
+  pt = Perceptron()
+  
+  iterations = int(args[1])
+  splits = pt.crossValidationSplits(args[0])
+  avgAccuracy = 0.0
+  fold = 0
+  for split in splits:
+    classifier = Perceptron()
+    accuracy = 0.0
+    classifier.train(split,iterations)
+  
+    for example in split.test:
+      words = example.words
+      guess = classifier.classify(words)
+      if example.klass == guess:
+        accuracy += 1.0
+
+    accuracy = accuracy / len(split.test)
+    avgAccuracy += accuracy
+    print '[INFO]\tFold %d Accuracy: %f' % (fold, accuracy) 
+    fold += 1
+  avgAccuracy = avgAccuracy / fold
+  print '[INFO]\tAccuracy: %f' % avgAccuracy
+    
+    
+def classifyDir(trainDir, testDir,iter):
+  classifier = Perceptron()
+  trainSplit = classifier.trainSplit(trainDir)
+  iterations = int(iter)
+  classifier.train(trainSplit,iterations)
+  testSplit = classifier.trainSplit(testDir)
+  #testFile = classifier.readFile(testFilePath)
+  accuracy = 0.0
+  for example in testSplit.train:
+    words = example.words
+    guess = classifier.classify(words)
+    if example.klass == guess:
+      accuracy += 1.0
+  accuracy = accuracy / len(testSplit.train)
+  print '[INFO]\tAccuracy: %f' % accuracy
+    
+def main():
+  (options, args) = getopt.getopt(sys.argv[1:], '')
+  
+  if len(args) == 3:
+    classifyDir(args[0], args[1], args[2])
+  elif len(args) == 2:
+    test10Fold(args)
+
+if __name__ == "__main__":
+    main()
